77365f897bc58e4ba1755212bc7e00431f2c9c61
Jorge Luis Lopez Barrera <jorge@Jorges-MacBook-Pro.local>
2024-09-02 09:26:17 -0600

    join-as-vet

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -90,7 +90,7 @@ const Header: React.FC = () => {
   }, []);
 
   const handleButtonClick = () => {
-    window.location.href = 'https://wa.link/2lfoqu';
+    window.location.href = '/join-as-vet';
   };
 
   return (
@@ -98,10 +98,10 @@ const Header: React.FC = () => {
       <img src="/header.svg" alt="Header SVG" className={styles.headerImage} />
       <div className={styles.texts}>
         <h1 className={styles.title}>La plataforma inteligente para el cuidado de tu animal de compañía.</h1>
-        <h2 className={styles.subtitle}>Call a Vet es una plataforma impulsada por Inteligencia Artificial que dispone para ti atención veterinaria especializada a un mensaje de distancia.{/* <br></br><br></br>
-        Si eres un profesional de la medicina veterinaria que busca incrementar su actividad laboral, ingresos y pacientes desde el lugar que desees trabajar, esta plataforma es para ti. */}</h2>
+        <h2 className={styles.subtitle}>Call a Vet es una plataforma impulsada por Inteligencia Artificial que dispone para ti atención veterinaria especializada a un mensaje de distancia.<br></br><br></br>
+        Si eres un profesional de la medicina veterinaria que busca incrementar su actividad laboral, ingresos y pacientes desde el lugar que desees trabajar, esta plataforma es para ti.</h2>
       </div>
-      <button className={styles.ctaButton} onClick={handleButtonClick}>Recibe asistencia ahora</button>
+      <button className={styles.ctaButton} onClick={handleButtonClick}>¡Únete Ya!</button>
       <a href="/" className={styles.smallLink}>Términos y condiciones</a>
       {icons.map((icon, index) => (
         <img key={index} src={`/${icon}`} alt={`Icon ${index + 1}`} className={styles.icon} />
